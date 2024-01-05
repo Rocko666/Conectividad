d465b8acf11cad5d0cf110764ca8244ef66e4147
Cristian Ortiz <cristian.ortiz.1990@gmail.com>
2024-01-05 16:24:04 -0500

    test

---
+++ bin/OTC_T_SQLSERVER.sh
@@ -0,0 +1,92 @@
+set -e
+#########################################################################################################
+# NOMBRE: OTC_T_V_BAJAS_SERVICIOS.sh			     	      								            #
+# DESCRIPCION:																							#
+#   Shell que extrae la información de la tabla v_bajas_servicios_DAS de SQLServer a Hive				#
+# AUTOR: Karina Castro - Softconsulting                            										#
+# FECHA CREACION: 2022-09-12   																			#
+# PARAMETROS DEL SHELL                            													    #
+# N/A  												    						                        #
+#########################################################################################################
+# MODIFICACIONES																						#
+# FECHA  		AUTOR     		DESCRIPCION MOTIVO														#
+# 11/07/2023    Cristian Ortiz  Control errores, estandares Cloudera, cambio conexion SQL SERVER        #
+#########################################################################################################
+##############
+# VARIABLES #
+##############
+ENTIDAD=D_TESTSQLSERVER
+
+###########################################################################################################################################################
+echo `date '+%Y-%m-%d %H:%M:%S'`" INFO: Parametros del SPARK GENERICO" 
+###########################################################################################################################################################
+VAL_KINIT=`mysql -N  <<<"select valor from params where ENTIDAD = 'SPARK_GENERICO' AND parametro = 'VAL_KINIT';"`
+$VAL_KINIT
+VAL_RUTA_SPARK=`mysql -N  <<<"select valor from params where ENTIDAD = 'SPARK_GENERICO' AND parametro = 'VAL_RUTA_SPARK';"`
+
+#PARAMETROS QUE RECIBE LA SHELL
+VAL_RUTA=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_RUTA';"`
+VAL_ESQUEMA=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_ESQUEMA';"`
+VAL_TABLA=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_TABLA';"`
+VAL_URL=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_URL';"`
+VAL_DATABASE=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_DATABASE';"`
+VAL_USUARIO=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_USUARIO';"`
+VAL_PASSWORD=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_PASSWORD';"`
+VAL_ESQUEMA_SQLSERVER=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_ESQUEMA_SQLSERVER';"`
+VAL_TABLA_SQLSERVER=`mysql -N  <<<"select valor from params_des where ENTIDAD = '"$ENTIDAD"' AND parametro = 'VAL_TABLA_SQLSERVER';"`
+
+#PARAMETROS CALCULADOS Y AUTOGENERADOS
+VAL_BD=$VAL_ESQUEMA.$VAL_TABLA
+VAL_BD_SQLSERVER=$VAL_ESQUEMA_SQLSERVER.$VAL_TABLA_SQLSERVER
+VAL_DIA=`date '+%Y%m%d'` 
+VAL_HORA=`date '+%H%M%S'` 
+VAL_LOG=$VAL_RUTA/log/sqlservertest_$VAL_DIA$VAL_HORA.log
+
+#VALIDACION DE PARAMETROS INICIALES
+if  [ -z "$ENTIDAD" ] || 
+    [ -z "$VAL_RUTA" ] || 
+    [ -z "$VAL_ESQUEMA" ] || 
+    [ -z "$VAL_TABLA" ] || 
+    [ -z "$VAL_URL" ] || 
+    [ -z "$VAL_DATABASE" ] || 
+    [ -z "$VAL_USUARIO" ] || 
+    [ -z "$VAL_PASSWORD" ] || 
+    [ -z "$VAL_ESQUEMA_SQLSERVER" ] || 
+    [ -z "$VAL_TABLA_SQLSERVER" ] || 
+    [ -z "$VAL_RUTA_SPARK" ] || 
+    [ -z "$VAL_LOG" ] ; then
+	echo " ERROR: - uno de los parametros esta vacio o nulo"
+	exit 1
+fi
+
+#INICIO DEL PROCESO
+echo "==== Inicia extraccion tabla V_BAJAS_SERVICIOS_DAS de SQLServer ===="`date '+%Y%m%d%H%M%S'` 2>&1 &>> $VAL_LOG
+
+#REALIZA LA TRANSFERENCIA DE LOS ARCHIVOS DESDE EL SERVIDOR FTP A RUTA LOCAL EN BIGDATA
+echo "==== Ejecuta archivo spark otc_t_v_bajas_servicios.py que extrae informacion de SQL SERVER a Hive ===="`date '+%Y%m%d%H%M%S'` 2>&1 &>> $VAL_LOG
+$VAL_RUTA_SPARK \
+--conf spark.port.maxRetries=100 \
+--master local \
+--executor-memory 32G \
+--num-executors 8 \
+--executor-cores 4 \
+--driver-memory 32G \
+--jars /var/opt/tel_lib/sqljdbc42.jar \
+$VAL_RUTA/python/otc_t_v_sqlservertest.py \
+--vUrl=$VAL_URL \
+--vDatabase=$VAL_DATABASE \
+--vUid=$VAL_USUARIO \
+--bd=$VAL_BD \
+--vPwd=$VAL_PASSWORD \
+--vTabla=$VAL_BD_SQLSERVER 2>&1 &>> $VAL_LOG
+
+#VALIDA EJECUCION DEL ARCHIVO SPARK
+error_spark=`egrep 'SyntaxError:|pyodbc.InterfaceError:|Caused by:|pyspark.sql.utils.ParseException|AnalysisException:|NameError:|IndentationError:|Permission denied:|ValueError:|ERROR:|error:|unrecognized arguments:|No such file or directory|Failed to connect|Could not open client' $VAL_LOG | wc -l`
+if [ $error_spark -eq 0 ];then
+	echo "==== OK - La ejecucion del archivo spark otc_t_v_bajas_servicios.py es EXITOSO ===="`date '+%H%M%S'` 2>&1 &>> $VAL_LOG
+	else
+	echo "==== ERROR: - En la ejecucion del archivo spark otc_t_v_bajas_servicios.py ====" 2>&1 &>> $VAL_LOG
+	exit 1
+fi
+
+echo "==== Finaliza extraccion tabla V_BAJAS_SERVICIOS_DAS de SQLServer ===="`date '+%Y%m%d%H%M%S'` 2>&1 &>> $VAL_LOG
---
+++ python/otc_t_v_sqlservertest.py
@@ -0,0 +1,83 @@
+# -- coding: utf-8 --
+#########################################################################################################
+# NOMBRE: otc_t_v_bajas_servicios.py											                        #
+# DESCRIPCION:																							#
+#  Lee la tabla v_bajas_servicios_DAS de la base externa SQL Server y la carga a Hive					#
+# AUTOR: Karina Castro - Softconsulting	                            									#
+# FECHA CREACION: 2022-09-19   																			#
+#########################################################################################################
+from pyspark import SparkContext, SparkConf, SQLContext
+import pyodbc
+import pandas as pd
+import sys
+import argparse
+import datetime
+import time
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql import functions as F
+from pyspark.sql import HiveContext
+
+# Genericos 
+sys.path.insert(1,'/var/opt/tel_spark')
+from messages import *
+from functions import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--vUrl', required=True, type=str)
+parser.add_argument('--vDatabase', required=True, type=str)
+parser.add_argument('--vUid', required=True, type=str)
+parser.add_argument('--bd', required=True, type=str)
+parser.add_argument('--vPwd', required=True, type=str)
+parser.add_argument('--vTabla', required=True, type=str)
+parametros = parser.parse_args()
+vUrl = parametros.vUrl
+vDatabase = parametros.vDatabase
+vUid = parametros.vUid
+bd = parametros.bd
+vPwd = parametros.vPwd
+vTabla = parametros.vTabla
+
+desde = time.time()
+
+spark = SparkSession\
+    .builder\
+    .appName("OTC_T_V_BAJAS_SERVICIOS")\
+    .enableHiveSupport()\
+    .getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
+sqlContext = SQLContext(sc)
+
+query = "(SELECT * FROM "+vTabla +" where [ID_FECHA_LLAMADA] >= 20231201 and [ID_FECHA_LLAMADA] <= 20231221) as c"
+
+df = sqlContext.read.format("jdbc") \
+	.option("url",vUrl).option("dbtable", query) \
+	.option("user", vUid).option("password", vPwd).option("driver","com.microsoft.sqlserver.jdbc.SQLServerDriver").load()
+
+df = df.withColumn('nomcli', F.regexp_replace('nomcli', '\t', ''))
+
+if df.limit(1).count <=0:
+        exit(etq_nodata(msg_e_df_nodata(str('df'))))
+else:
+    try:
+        timestart_tbl = datetime.now()
+        print(etq_info(msg_i_insert_hive(bd)))
+        df.repartition(1).write.format("parquet").mode("overwrite").saveAsTable(bd)
+        df.printSchema()
+        print(etq_info(msg_t_total_registros_hive(bd,str(df.count())))) 
+        timeend_tbl = datetime.now()
+        print(etq_info(msg_d_duracion_hive(bd,vle_duracion(timestart_tbl,timeend_tbl))))
+    except Exception as e:       
+        exit(etq_error(msg_e_insert_hive(bd,str(e))))
+
+print("Escritura Exitosa de la tabla "+bd)
+
+spark.stop()
+hasta = time.time()
+duracion = hasta - desde
+print("Duracion: {vDuracion}".format(vDuracion=duracion))
